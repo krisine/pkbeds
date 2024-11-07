9637b340b20c47724742caf0554cb38a017b8236
Romance <faaa@live.com>
2024-11-07 10:21:03 +0800

    update

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,20 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-07-25'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-11-07'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
-)
-
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
-)
-
-function assertValue(v, errorMessage) {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
